a879a0d5d90a34d057f6675f8d7300eec75b7846
Victor's Laptop <vlp47226@uga.edu>
2024-12-31 18:05:28 -0500

    Worked on get_data script, still a few things to do, added to ReadMe

--- Predicting NFL Career Length via Combine Stats/README.md
+++ Predicting NFL Career Length via Combine Stats/README.md
@@ -4,4 +4,8 @@ The dataset: NFL_Combine_and_pro_day_data_(1987-2021).csv
 
 The headers: Year,Name,College,POS,Height (in),Weight (lbs),Wonderlic,40 Yard,Bench Press,Vert Leap (in),Broad Jump (in),Shuttle,3Cone
 
+
+TODO:
+    Explore the dataset to see duplicates, data distributions, number of unique schools, etc.
+    
 The first step is to find all the available data regarding the length of as many of the players in the original NFL combine dataset
--- Predicting NFL Career Length via Combine Stats/get_data.py
+++ Predicting NFL Career Length via Combine Stats/get_data.py
@@ -1,19 +1,75 @@
+import re
+from time import sleep
+from urllib import response
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 # Load your CSV
 players = pd.read_csv('NFL_Combine_and_pro_day_data_(1987-2021).csv')
+stem = "https://www.pro-football-reference.com/players/"
+'''
+TODO:
+    If "St.", combine "St." with the next word
+        Unless first name is "Amon-Ra", then last name should be "Stxx"
+    Add successful URL to dictionary to avoid multiple players with the same name problem
 
+Done:
+    if last name length < 4, pad with "x" done
+    Problem: (A-Z).(A-Z). is sometimes "(A-Z)." or "(A-Z)(A-Z)
+    hyphen(-) should be removed
+    apostrophe(') should be removed
+    
+'''
+checkedNames = {}
 # Function to scrape career years from Pro Football Reference
 def get_player_career_length(player_name):
     try:
-        # Format the player name for the URL
-        search_name = player_name.lower().replace(' ', '-')
-        url = f"https://www.pro-football-reference.com/players/{search_name[0]}/{search_name[:4]}{search_name.split()[-1][:2]}00.htm"
-        response = requests.get(url)
-        if response.status_code != 200:
-            return None
+        sleep(4) # Wait 4 seconds between requests
+        player_name = player_name.replace("-", "").replace("'", "") # Replace hyphen with space
+        search_name = player_name.split() # Split the player name for the URL
+        if len(search_name[-1]) < 4:
+            search_name[-1] = search_name[-1] + "x"*(4-len(search_name[-1]))
+
+        url = f"{stem}{search_name[-1][0]}/{search_name[-1][:4]}{search_name[0][:2]}00.htm"
+        if search_name[0] == "Amon-Ra":
+            url = f"{stem}{search_name[-1][0]}/{search_name[-1][:4]}Stxx00.htm"
+        if "." in search_name[0]:
+            url = f"{stem}{search_name[-1][0]}/{search_name[-1][:4]}{search_name[0][0]}{search_name[0][2]}00.htm" #all the (A-Z). did not have a number different than 00
+            #response = requests.get(None)
+            if checkedNames.get(url) == False:
+                #response = requests.get(url)
+                response = requests.get(None)
+                checkedNames[url] = True
+                sleep(4) # Wait 4 seconds between requests
+            elif response.status_code == 404 or checkedNames.get(url) == False: 
+               url = f"{stem}{search_name[-1][0]}/{search_name[-1][:4]}{search_name[0][:2]}00.htm"
+               checkedNames[url] = True
+
+        print(url)
+        #check if url is visited already
+        if url not in checkedNames:
+            #response = requests.get(url)
+            response = requests.get(None)
+            print("Novel URL")
+        else:
+            #response = requests.get(None)
+            response = requests.get(None)
+            print("Used URL")
+        checkOtherNumbers = 0
+        """If the player's URL doesn't exist, try other numbers"""
+        while response.status_code == 404:
+            newNum = str(checkOtherNumbers)
+            newNum = "0"+newNum if newNum < 10 else newNum
+            sleep(4) # Wait 4 seconds between requests
+            newurl = url.replace("00", newNum)
+            if checkedNames.get(newurl) == False:
+                #response = requests.get(newurl)
+                response = requests.get(None)
+                checkedNames[newurl] = True
+            if newNum > 99:
+                return None
+            checkOtherNumbers += 1
         
         # Parse the HTML
         soup = BeautifulSoup(response.content, 'html.parser')
